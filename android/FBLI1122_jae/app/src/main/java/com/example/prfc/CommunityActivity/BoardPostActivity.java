d8285b5e971bae9520682f6e7bbc13f9b4b70741
박창섭 <snp0315@ajou.ac.kr>
2018-12-01 20:46:55 +0900

    사진업로드

--- android/FBLI1122_jae/app/src/main/java/com/example/prfc/CommunityActivity/BoardPostActivity.java
+++ android/FBLI1122_jae/app/src/main/java/com/example/prfc/CommunityActivity/BoardPostActivity.java
@@ -1,12 +1,13 @@
 package com.example.prfc.CommunityActivity;
-
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,9 +19,25 @@ import android.app.Activity;
 import com.example.prfc.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import org.json.JSONObject;
-
+import java.io.DataOutputStream;
 import java.io.OutputStream;
 import java.io.File;
 import java.net.HttpURLConnection;
@@ -35,13 +52,14 @@ public class BoardPostActivity extends AppCompatActivity{
     FirebaseUser user;
     Button Btn_select;
 
-    //private Uri photoUri;
-    private String currentPhotoPath;//실제 사진 파일 경로
-    String mImageCaptureName;//이미지 이름
+    String imgPath ="";
+    String imgName="";
 
+    int serverResponseCode = 0;
     private final int CAMERA_CODE = 1111;
     private final int GALLERY_CODE=1112;
-
+    final int REQ_CODE_SELECT_IMAGE=100;
+    File imgFile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,23 +71,28 @@ public class BoardPostActivity extends AppCompatActivity{
         Btn_select = (Button)findViewById(R.id.selectPhoto);
         btn_upload = (Button) findViewById(R.id.btn_upload);
         user = FirebaseAuth.getInstance().getCurrentUser();
+
         btn_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                base64_string = getStringFromBitmap(imageview.getDrawingCache());
-                new PostTask().execute();
-                startActivity(new Intent(BoardPostActivity.this, CommunityActivity.class));
+                new Thread() {
+                    public void run() {
+                        doFileUpload();
+                    }
+                }.start();
             }
         });
         Btn_select.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 selectGallery();
+
             }
         });
     }
 
-
+    //Post 데이터
     public class PostTask extends AsyncTask<String, Void, String> {
 
         String clientKey = "#########################";;
@@ -90,6 +113,7 @@ public class BoardPostActivity extends AppCompatActivity{
             super.onPostExecute(s);
         }
     }
+    //Post 이미지
     public class ImagePost extends AsyncTask<String, Void, String> {
 
         String clientKey = "#########################";;
@@ -108,11 +132,11 @@ public class BoardPostActivity extends AppCompatActivity{
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            //File image =
+            doFileUpload();
         }
     }
 
-    //Delete
+    //데이터 포맷만들어서 보내기
     public String connPOST(String serverURL, String... params){
 
         String baseURL = "http://13.209.15.179:50000/";
@@ -120,14 +144,10 @@ public class BoardPostActivity extends AppCompatActivity{
         int responseStatusCode;
 
         try {
-//
-//            jsonObject.put("_id","");
-//            jsonObject.put("boardid","");
+
             jsonObject.put("title",title.getText().toString());
             jsonObject.put("userid",user.getUid());
             jsonObject.put("content",content.getText().toString());
-//            jsonObject.put("comment","");
-//            jsonObject.put("Image","");
 
             URL url = new URL(serverURL);
             HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
@@ -156,6 +176,9 @@ public class BoardPostActivity extends AppCompatActivity{
         }
     }
 
+
+
+    //갤러리 접근
     private void selectGallery() {
 
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -163,50 +186,129 @@ public class BoardPostActivity extends AppCompatActivity{
         intent.setType("image/*");
         startActivityForResult(intent, GALLERY_CODE);
     }
-    @Override
+    @Override //갤러리 접근 후 동작
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
+        super.onActivityResult(requestCode, resultCode, data);
 
         Toast.makeText(getBaseContext(), "resultCode : "+resultCode,Toast.LENGTH_SHORT).show();
-
-//        if(requestCode == REQ_CODE_SELECT_IMAGE)
-//        {
-            if(resultCode==Activity.RESULT_OK)
-            {
-                try {
-                    //Uri에서 이미지 이름을 얻어온다.
-                    String name_Str = getImageNameToUri(data.getData());
-
-                    //이미지 데이터를 비트맵으로 받아온다.
-                    Bitmap image_bitmap 	= Images.Media.getBitmap(getContentResolver(), data.getData());
-
-                    //배치해놓은 ImageView에 set
-                    imageview.setImageBitmap(image_bitmap);
-
-
-                    //Toast.makeText(getBaseContext(), "name_Str : "+name_Str , Toast.LENGTH_SHORT).show();
-
-
+        if(resultCode==Activity.RESULT_OK)
+        {
+            try {
+                //Uri에서 이미지 이름을 얻어온다.
+                String name_Str = getImageNameToUri(data.getData());
+                //이미지 데이터를 비트맵으로 받아온다.
+                Bitmap image_bitmap = Images.Media.getBitmap(getContentResolver(), data.getData());
+                //배치해놓은 ImageView에 set
+                imageview.setImageBitmap(image_bitmap);
+                Toast.makeText(getBaseContext(), "name_Str : "+name_Str , Toast.LENGTH_SHORT).show();
+                imgFile = new File(imgPath);
+                if(!imgFile.isFile()){
+                    System.out.println("****************************_파일없음");
                 }
+                else{
+                    System.out.println("****************************_"+imgFile.getName());
+                }
+            }
                  catch (Exception e)
                 {
                     e.printStackTrace();
                 }
-            }
-//        }
+        }
+
     }
     public String getImageNameToUri(Uri data)
     {
         String[] proj = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(data, proj, null, null, null);
+        Cursor cursor = getContentResolver().query(data, proj, null, null, null);
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
 
         cursor.moveToFirst();
 
-        String imgPath = cursor.getString(column_index);
-        String imgName = imgPath.substring(imgPath.lastIndexOf("/")+1);
+        imgPath = cursor.getString(column_index);
+        imgName = imgPath.substring(imgPath.lastIndexOf("/")+1);
+        EditText t = (EditText)findViewById(R.id.debug);
+        t.setText(imgName + "||" + imgPath);
 
         return imgName;
     }
 
+    public void doFileUpload() {
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        System.out.println("doFileUpload");
+            try {
+                System.out.println("********************************1  "+imgPath+"엥");
+                System.out.println("********************************2"+Environment.getExternalStorageDirectory().getAbsolutePath());
+                FileInputStream mFileInputStream = new FileInputStream(imgFile);
+                System.out.println("********************************2");
+                URL connectUrl = new URL("http://13.209.15.179:50000/upload");
+                Log.d("Test", "mFileInputStream  is " + mFileInputStream);
+
+                // open connection
+                HttpURLConnection conn = (HttpURLConnection)connectUrl.openConnection();
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setUseCaches(false);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+                // write data
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"userfile\";filename=\"" + imgName+"\"" + lineEnd);
+                dos.writeBytes(lineEnd);
+
+                int bytesAvailable = mFileInputStream.available();
+                int maxBufferSize = 1024;
+                int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                byte[] buffer = new byte[bufferSize];
+                int bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+
+                Log.d("Test", "image byte is " + bytesRead);
+
+                // read image
+                while (bytesRead > 0) {
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = mFileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = mFileInputStream.read(buffer, 0, bufferSize);
+                }
+                System.out.println("********************************2");
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // close streams
+                Log.e("Test" , "File is written");
+                mFileInputStream.close();
+                dos.flush(); // finish upload...
+                System.out.println("********************************13");
+                // get response
+                int ch;
+                InputStream is = conn.getInputStream();
+                StringBuffer b =new StringBuffer();
+                while( ( ch = is.read() ) != -1 ){
+                    b.append( (char)ch );
+                }
+                String s=b.toString();
+                Log.e("Test", "result = " + s);
+                //mEdityEntry.setText(s);
+                dos.close();
+
+            } catch (FileNotFoundException e){
+                System.out.println(e.getStackTrace());
+                System.out.println("파일없어*********************************************");
+            }catch (Exception e) {
+                Log.d("Test", "exception " + e.getMessage());
+
+                System.out.println(e.toString()+"다른거야*********************************************");
+                // TODO: handle exception
+            }
+
+        }
+
+
+
 }
+
